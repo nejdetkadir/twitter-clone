a917b54e35c1803f952acde6da19f47d6aa5629c
nejdetkadir <nejdetkadir.550@gmail.com>
2021-04-10 12:29:40 +0300

    Add devise gem

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  # validates
+  validates :fullname, presence: true, length: {minimum: 3, maximum: 30}
+  validates :username, presence: true, uniqueness: {case_sensitive: false}
+  validates :location, length: {minimum: 2, maximum: 20}
+  validates :website, format: {with: /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/, message: "Please check your URL" }
+  validates :bio, length: {maximum:160}
+
+  # mounts
+
+
+  # associations
+
+  
+end
