d0a22af3e32a81b6b28684f906db03748e3a459d
nejdetkadir <nejdetkadir.550@gmail.com>
2021-04-11 16:53:40 +0300

    Add login process

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,16 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def check_signed_in
+    redirect_to feed_path if signed_in?
+  end
+
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname, :profile_picture, :bio, :username])
+    end
+
+    def after_sign_in_path_for(resource)
+      feed_path
     end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!, only: [:feed]
+  before_action :check_signed_in, only: [:home]
+
   def home
   end
 
